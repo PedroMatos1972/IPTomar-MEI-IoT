d14cf41465d81c31451cedd89ef36de3f9db502e
Pedro Matos <pedromatos@outlook.pt>
2016-11-28 11:42:15 +0000

    Login - Admin e Users
    Criado a estrutura de registo de users e do admin….. falta o design,
    depois construir o backoffice e colocar as restrições de acesso, também
    fazer uma pagina de login para os users.

--- ProjectSubmit/config/routes.rb
+++ ProjectSubmit/config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  resources :projetos
-  
-  root "home#index"
+#---- users e admin backoffice
+  #get 'admin/index'
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
+  get 'usres/new'
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+# já constuido do anterior das páginas sem autenticação
+get 'home/index'
+resources :projetos
+root "home#index"
+resources :users
+  #root 'home#index', as: 'home_index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- ProjectSubmit/db/schema.rb
+++ ProjectSubmit/db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120211709) do
+ActiveRecord::Schema.define(version: 20161128103531) do
 
   create_table "projetos", force: :cascade do |t|
     t.string   "titulo"
     t.string   "resume"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "telefone"
+    t.string   "escola"
+    t.string   "funcao"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
